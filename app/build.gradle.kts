bc9eddb69c7d23dc4c89b9361f1abc32750eeb0b
TruongHoang11 <truongconghuyhoang1407@gmail.com>
2026-04-27 19:34:57 +0700

    function_update_profile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:34.12.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-database")
+    implementation("com.google.firebase:firebase-storage")
     // Materials
     implementation("com.google.android.material:material:1.9.0")
 }
